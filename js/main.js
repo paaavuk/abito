8f2a920430377c919e7ce578c0c1c414e23bc5b5
pavuk <paaavuk@outlook.com>
2021-07-28 10:31:11 +0300

    + single.page

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 6,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+const swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    thumbs: {
+        swiper: swiper,
+    },
+});
